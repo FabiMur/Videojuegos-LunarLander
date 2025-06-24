5b4903953ddda17e28aa0a1295a82823208e8fcc
Fabián Murgado <fabian@fabianmurgado.com>
2025-06-24 17:49:02 +0200

    Eliminado punteado al hacer zoom

--- code/dibujable.c
+++ code/dibujable.c
@@ -1,5 +1,6 @@
 #include "dibujable.h"
 #include <stdio.h>
+#include <windows.h>
 
 
 struct Dibujable* crearDibujable(const struct DibujableConstante* constante) {
@@ -55,24 +56,13 @@ void destruirArista(struct Arista* arista){
     free(arista);
 }
 
-
-// Algoritmo de Bresenham para rasterizar una línea
 void DrawLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color) {
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
-    int err = dx - dy;
-
-    while (1) {
-        SetPixel(hdc, x1, y1, color); // Dibuja el pixel actual
-
-        if (x1 == x2 && y1 == y2) break; // Si llegamos al final, salimos
-
-        int e2 = 2 * err;
-        if (e2 > -dy) { err -= dy; x1 += sx; }
-        if (e2 < dx) { err += dx; y1 += sy; }
-    }
+    HPEN pen = CreatePen(PS_SOLID, 1, color);
+    HPEN old = (HPEN)SelectObject(hdc, pen);
+    MoveToEx(hdc, x1, y1, NULL);
+    LineTo(hdc, x2, y2);
+    SelectObject(hdc, old);
+    DeleteObject(pen);
 }
 
 
